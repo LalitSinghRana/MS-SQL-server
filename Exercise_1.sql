9d7bd858977abb78b77f048a8aa9724d88a60351
Lalit Rana <lalit.rana@nagarro.com>
2021-08-01 18:18:42 +0530

    Till Ex 1-7 finished

---
+++ Exercise_1.sql
@@ -0,0 +1,74 @@
+USE AdventureWorks2017;
+
+-- 1-1
+
+DECLARE @Number_Of_Records INT;
+SELECT @Number_Of_Records = COUNT(*) FROM Sales.SalesPerson;
+PRINT 'The number of records in the [SalesPerson] table are ' + CAST(@Number_Of_Records AS VARCHAR(10));
+
+
+-- 1-2
+SELECT FirstName AS 'Given Name'
+	, LastName AS 'Surname'
+	FROM Person.Person
+	WHERE FirstName LIKE 'B%';
+
+
+-- 1-3
+SELECT p.FirstName
+	, p.LastName
+	, hr.JobTitle
+	FROM Person.Person AS p 
+	INNER JOIN HumanResources.Employee AS hr
+	ON p.BusinessEntityID = hr.BusinessEntityID
+	-- ORDER BY hr.JobTitle;
+	WHERE hr.JobTitle LIKE 'Design Engineer' 
+		OR hr.JobTitle LIKE 'Tool Designer' 
+		OR hr.JobTitle LIKE 'Marketing Assistant';
+
+
+-- 1-4
+SELECT TOP 1 p.Name
+	, p.Color
+	FROM Production.Product AS p
+	ORDER BY p.Weight DESC;
+
+SELECT p.Name										-- Another way to do
+	, p.Color										-- the same thing
+	FROM Production.Product AS p
+	WHERE p.Weight = (
+		SELECT MAX(Weight) 
+		FROM Production.Product);
+
+
+-- 1-5
+SELECT ISNULL(MaxQty, 0.00)
+	, Description
+	FROM Sales.SpecialOffer;
+
+
+-- 1-6
+SELECT AVG(AverageRate) AS '2005_USDtoGBP_Avg'
+	FROM Sales.CurrencyRate
+	WHERE FromCurrencyCode = 'USD' 
+		AND ToCurrencyCode = 'GBP' 
+		AND YEAR(CurrencyRateDate) = 2012;			--Using 2012 as AdventureWorks2017 doesn't contain data for 2005
+
+
+-- 1-7
+SELECT FirstName
+	, LastName
+	, ROW_NUMBER() OVER(ORDER BY (SELECT NULL)) AS 'S.No.'
+	FROM Person.Person
+	WHERE FirstName LIKE '%ss%';
+
+
+-- 1-8
+
+
+
+-- 1-9
+
+
+
+-- 1-10
